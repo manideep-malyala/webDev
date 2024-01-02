39fc7acbfc8337c03b7c405b9a09bf3b64f03fbc
manideep_malyala <manideepmalyala97@gmail.com>
2024-01-02 23:42:28 +0530

    the basics

---
+++ react intro/basic react app/app.js
@@ -0,0 +1,3 @@
+const heading = React.createElement("h1", {id: "heading"}, "msg created with react app.js version ! ");
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
